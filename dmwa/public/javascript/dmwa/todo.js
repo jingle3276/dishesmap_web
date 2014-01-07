c0892bdb651a0fa5d6de4447ca188900ffb34ebe
Ying Zhao <yzhao@amplify.com>
2014-01-07 00:14:36 -0500

    added goog.require module pattern

--- dmwa/public/javascript/dmwa/todo.js
+++ dmwa/public/javascript/dmwa/todo.js
@@ -1,28 +1,33 @@
+goog.provide('wz.dmwa.Todo');
+
+
+(function () {
 // Define a Todo Model
-var Todo = Backbone.Model.extend({
+	wz.dmwa.Todo = Backbone.Model.extend({
 
-	initialize: function() {
-      console.log('This model has been initialized.');
+		initialize: function() {
+	      console.log('This model has been initialized.');
 
-      this.on('change', function(){
-        console.log('Values for this model have changed.');
-      });
+	      this.on('change', function(){
+	        console.log('Values for this model have changed.');
+	      });
 
-  	}, 
+	  	}, 
 
-	// Default todo attribute values
-	defaults: {
-		content: 'default content'
-	},
+		// Default todo attribute values
+		defaults: {
+			content: 'default content'
+		},
 
-	setContent: function(newContent){
-		this.set({content: newContent});
-	},
+		setContent: function (newContent) {
+			this.set({content: newContent});
+		},
 
-	getContent: function(){
-		return this.get('content');
-	}
+		getContent: function () {
+			return this.get('content');
+		}
 
-});
+	});
 
-var myTodo = new Todo();
+}());
+
--- dmwa/public/javascript/dmwa/todo_view.js
+++ dmwa/public/javascript/dmwa/todo_view.js
@@ -1,32 +1,42 @@
-var TodoView = Backbone.View.extend({
+goog.provide('wz.dmwa.TodoView');
 
-  tagName:  'input',
+goog.require('wz.dmwa.Todo');
 
-  events: {
-    //"click input[type=button]": "doChange"
-    'click #change_button': 'doChange'
-  },
+(function () {
 
-  // Called when the view is first created
-  initialize: function() {
-    this.$el = $('#todo');
-    this.render();
-  },
+  var myTodo = wz.dmwa.Todo;
 
-  //
-  render: function() {
-    var compiled = _.template($("#todo_template").html(), this.model.toJSON());
-    this.$el.find("#display").html(compiled);
-  },
+  wz.dmwa.TodoView = Backbone.View.extend({
 
-  doChange: function(event) {
-    // Button clicked, you can access the element that was clicked with event.currentTarget
-    var content = this.$el.find("#input").val();
-    this.model.setContent(content);
-    this.render();
-  }
+    tagName:  'input',
 
-});
+    events: {
+      //"click input[type=button]": "doChange"
+      'click #change_button': 'doChange'
+    },
 
-// create a view for a todo
-var todoView = new TodoView({model: myTodo});
+    // Called when the view is first created
+    initialize: function() {
+      this.$el = $('#todo');
+      this.model = new myTodo();
+      this.render();
+    },
+
+    //
+    render: function() {
+      var compiled = _.template($("#todo_template").html(), this.model.toJSON());
+      this.$el.find("#display").html(compiled);
+    },
+
+    doChange: function(event) {
+      // Button clicked, you can access the element that was clicked with event.currentTarget
+      var content = this.$el.find("#input").val();
+      this.model.setContent(content);
+      this.render();
+    }
+
+  });
+  // create a view for a todo
+  todoView = new wz.dmwa.TodoView({model: myTodo});
+
+}());
