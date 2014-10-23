3b06cd49ef0a212386ded49d47ca6267aac5e371
Ying R Zhao <jingle3276@hotmail.com>
2014-10-22 23:30:49 -0400

    use the qunit test sample format to write test

--- dmwa/public/javascript/tests/app/models/test_dishdetail.js
+++ dmwa/public/javascript/tests/app/models/test_dishdetail.js
@@ -1,23 +1,18 @@
 goog.provide('wz.dmwa.tests.app.models.TestDishdetail');
-
 goog.require('wz.dmwa.app.models.Dishdetail');
 
 module('wz.dmwa.tests.app.models.TestDishdetail');
-(function () {
+QUnit.test("Dishdeail getRestaurantTel", function(assert) {
     var Dishdetail = wz.dmwa.app.models.Dishdetail;
 
-    test("Dishdetail model fields", function () {
-		var FIELDS = Dishdetail.prototype.FIELDS;
-        var attrs = {};
-        attrs[FIELDS.FOOD_TEXT] = 'food title';
-        attrs[FIELDS.BIZ_NAME] = 'biz name';
-        attrs[FIELDS.BIZ_ADDR] = 'addr';
-        attrs[FIELDS.BIZ_TEL] = '1234567890';
-	
-		var dd = new Dishdetail(attrs);
+	var FIELDS = Dishdetail.prototype.FIELDS;
+    var attrs = {};
+    attrs[FIELDS.FOOD_TEXT] = 'food title';
+    attrs[FIELDS.BIZ_NAME] = 'biz name';
+    attrs[FIELDS.BIZ_ADDR] = 'addr';
+    attrs[FIELDS.BIZ_TEL] = '1234567890';
 
+	var dd = new Dishdetail(attrs);
 
-		equal(dd.getRestaurantTel(), '(123) 456-7890', 'getRestaurantTel return correct format');
-    });
-
-}());
+	assert.equal(dd.getRestaurantTel(), '(123) 456-7890', 'getRestaurantTel return correct format');
+});
