fab5ecc69fccf1734a3908b11e08e56da488a856
Ying R Zhao <jingle3276@hotmail.com>
2014-10-25 02:01:57 -0400

    add sinon.js and an spy unit test as starting point

--- dmwa/public/javascript/tests/app/models/test_dishdetail.js
+++ dmwa/public/javascript/tests/app/models/test_dishdetail.js
@@ -1,18 +1,27 @@
 goog.provide('wz.dmwa.tests.app.models.TestDishdetail');
 goog.require('wz.dmwa.app.models.Dishdetail');
 
-module('wz.dmwa.tests.app.models.TestDishdetail');
-QUnit.test("Dishdeail getRestaurantTel", function(assert) {
-    var Dishdetail = wz.dmwa.app.models.Dishdetail;
+(function () {
+	var Dishdetail = wz.dmwa.app.models.Dishdetail;
 
-	var FIELDS = Dishdetail.prototype.FIELDS;
-    var attrs = {};
-    attrs[FIELDS.FOOD_TEXT] = 'food title';
-    attrs[FIELDS.BIZ_NAME] = 'biz name';
-    attrs[FIELDS.BIZ_ADDR] = 'addr';
-    attrs[FIELDS.BIZ_TEL] = '1234567890';
+	QUnit.module( "wz.dmwa.tests.app.models.TestDishdetail", {
+		setup: function() {
+			var spy = sinon.spy();
+		},
+		teardown: function() {
+			//
+	  	}
+	});
 
-	var dd = new Dishdetail(attrs);
+	QUnit.test("getRestaurantTel", function(assert) {
+		var FIELDS = Dishdetail.prototype.FIELDS;
+	    var attrs = {};
+	    attrs[FIELDS.FOOD_TEXT] = 'food title';
+	    attrs[FIELDS.BIZ_NAME] = 'biz name';
+	    attrs[FIELDS.BIZ_ADDR] = 'addr';
+	    attrs[FIELDS.BIZ_TEL] = '1234567890';
 
-	assert.equal(dd.getRestaurantTel(), '(123) 456-7890', 'getRestaurantTel return correct format');
-});
+		var dd = new Dishdetail(attrs);
+		assert.equal(dd.getRestaurantTel(), '(123) 456-7890', 'getRestaurantTel return correct format');
+	});
+}());
