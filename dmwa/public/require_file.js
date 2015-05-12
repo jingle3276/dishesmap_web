98c15f626e964b59aa5ec1b964597a234e832187
Ying Zhao <yingzhao@Ying-Zhao.local>
2015-05-12 18:07:23 -0400

    js_minificatin: change task to use closurebuilder to compile js

---
+++ dmwa/public/require_file.js
@@ -0,0 +1,5 @@
+goog.provide('wz.dmwa.require_file.js');
+goog.require('wz.dmwa.lib.templates.index'); //global compileTemplates
+goog.require('wz.dmwa.core.templates.index');
+goog.require('wz.dmwa.app.templates.index');
+goog.require('wz.dmwa.app.routers.AppRouter'); //load all dependency files
